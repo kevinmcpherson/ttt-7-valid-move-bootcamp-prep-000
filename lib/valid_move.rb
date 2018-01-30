489b907b73f836bec1ca8d7678a6c981b2513fdf
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-30 19:23:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,6 +13,6 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between(0, 8) && !position_taken(board, index)
+  index.between(0, 8) && !position_taken?(board, index)
     true
 end
